f4d5edec92059ea248ac1f4d4da5222af2a9f7f1
polansky <zdrav.polansky@gmail.com>
2020-08-24 19:14:51 +0300

    added text

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 text = "Дана строка."
 print(1, text[3-1])
-print(2, text[-2])
-print(3, text[0:5])
-print(4, text[0:len(text)-2])
-print(5, text[::2])
-print(6, text[1::2])
-print(7, text[len(text)-1::-1])
-print(8, text[len(text)-1::-2])
-print(9, len(text))
+a=10
+b=20
+print (a+b)
